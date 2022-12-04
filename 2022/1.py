3b766dad88fd2d7cf8e39df0fbfd853aafc02ce7
frode <frode.thorsen.borseth@softwarelab.no>
2022-12-04 07:32:03 +0100

    Day 1 - Top n sums

---
+++ 2022/1.py
@@ -0,0 +1,33 @@
+def get_top_n(numbers, n):
+    top_group = numbers[:1]
+    for number in numbers[1:]:
+        i = 0
+        while any(top < number for top in top_group[i:]):
+            i += 1
+        top_group = (top_group[:i] + [number] + top_group[i:])[-n:]
+    return top_group
+
+
+def solve():
+    elf_inventory_lists = [
+        [int(line) for line in chunk.splitlines()]
+        for chunk in open("inputs/1", "r").read().split("\n\n")
+    ]
+    totals = [sum(inv) for inv in elf_inventory_lists]
+
+    # sorted_totals = sorted(totals)
+    # yield sorted_totals[-1]
+    # yield sum(sorted_totals[-3:])
+    yield sum(get_top_n(totals, 1))
+    yield sum(get_top_n(totals, 3))
+
+
+def solutions():
+    yield 71924
+    yield 210406
+
+
+if __name__ == "__main__":
+    from helpers import main_template
+
+    main_template(solve, solutions)
