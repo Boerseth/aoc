0099658ca2c9758cb950bb79cd4392b443ef12a2
frode <frode.thorsen.borseth@softwarelab.no>
2022-12-06 07:01:23 +0100

    Pretty up solution 5

--- 2022/5.py
+++ 2022/5.py
@@ -1,26 +1,31 @@
-def parse_input(text):
-    end = text.find("\n 1")
-    diagram_lines, move_lines = text[:end].splitlines(), text[end:].splitlines()[3:]
-    crates = [list("".join(stack).strip()) for stack in list(zip(*diagram_lines[::-1]))[1::4]]
-    moves_1_indexed = [map(int, line.split()[1::2]) for line in move_lines]
-    moves = [(num, from_pos - 1, to_pos - 1) for num, from_pos, to_pos in moves_1_indexed]
-    return crates, moves
+def parse_diagram(diagram_text):
+    diagram_lines = diagram_text.splitlines()[::-1]
+    return ["".join(stack).strip() for stack in list(zip(*diagram_lines))[1::4]]
 
 
-def rearrange_crates(crate_stacks, move_steps, move_multiple=False):
+def parse_moves(move_text):
+    moves_1_index = [map(int, line.split()[1::2]) for line in move_text.splitlines()]
+    return [(n, from_pos - 1, to_pos - 1) for n, from_pos, to_pos in moves_1_index]
+
+
+def parse(text):
+    diagram_end = text.find("\n\n")
+    return parse_diagram(text[:diagram_end]), parse_moves(text[diagram_end + 2 :])
+
+
+def rearrange(stacks, moves, move_multiple=False):
     order = 1 if move_multiple else -1
-    for number, from_pos, to_pos in move_steps:
-        crate_stacks[to_pos] += crate_stacks[from_pos][-number:][::order]
-        crate_stacks[from_pos] = crate_stacks[from_pos][:-number]
-    return "".join(c[-1] for c in crate_stacks)
+    for number, from_pos, to_pos in moves:
+        stacks[to_pos] += stacks[from_pos][-number:][::order]
+        stacks[from_pos] = stacks[from_pos][:-number]
+    return stacks
 
 
 def solve():
     with open("inputs/5", "r") as f:
         text = f.read()
-    crate_stacks, move_steps = parse_input(text)
-    yield rearrange_crates([[c for c in s] for s in crate_stacks], move_steps, move_multiple=False)
-    yield rearrange_crates([[c for c in s] for s in crate_stacks], move_steps, move_multiple=True)
+    yield "".join(stack[-1] for stack in rearrange(*parse(text), False))
+    yield "".join(stack[-1] for stack in rearrange(*parse(text), True))
 
 
 def solutions():
