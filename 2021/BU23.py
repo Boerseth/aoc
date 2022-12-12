7b3d2cbe6603008e9cf5f33a00024de88339476b
frode <frode.thorsen.borseth@softwarelab.no>
2022-12-12 10:13:08 +0100

    23 wip

--- 2021/BU23.py
+++ 2021/BU23.py
@@ -77,62 +77,29 @@ def _get_legal_destinations(diagram, amphipod, origin, previous, current, distan
     return legal_destinations
 
 
-def can_go_straight_home(diagram, amphipod, pos):
-    r, c = pos
-    c_room = 
-
-def get_hallway_positions_incr(start, end):
-    if start <= 1:
-        yield 1
-    i = 2
-    while i <= end:
-        yield i
-        i += 2
-    if end == 11:
-        yield 11
-
-
-def get_hallway_positions_decr(start, end):
-    if start >= 11:
-        yield 11
-    i = 10
-    while i >= end:
-        yield i
-        i -= 2
-    if end <= 1:
-        yield 1
-
 def get_legal_destinations(diagram, max_depth, amphipod, pos):
     r, c = pos
     c_room = ROOM_NUMBER[amphipod]
     if r > 1:
-        range_in_front = range(r - 1, 1, -1)
-        if any(diagram[(r_i, c)] != "." for r_i in range_in_front):
-            return []
         if c == c_room:
-            range_behind = range(1 + max_depth, r - 1, -1)
-            if all(diagram[(r_i, c)] == amphipod for r_i in range_behind):
             return []
-
-        range_right = list(range(c + 1, 11, 2)) + [11]
-        range_left = list(range(c - 1, 0, -2)) + [1]
         destinations = []
-        for c_i in range_right:
-            if diagram[(1, c_i)] != ".":
+        for c_i in range(c + 1, 12):
+            if diagram[get_index((1, c_i))] != ".":
                 break
             destinations.append(((1, c_i), r - 1 + abs(c_i - c)))
-        for c_i in range_left:
-            if diagram[(1, c_i)] != ".":
+        for c_i in range(c - 1, 0, -1):
+            if diagram[get_index((1, c_i))] != ".":
                 break
             destinations.append(((1, c_i), r - 1 + abs(c_i - c)))
         return destinations
     # r == 1
     range_to_check = (c + 1 + (c > 1), c_room) if c < c_room else (c_room + 1, c)
     for c_i in range(*range_to_check, 2):
-        if diagram[(1, c_i)] != ".":
+        if diagram[get_index((1, c_i))] != ".":
             return []
     for r_i in range(1 + max_depth, 1, -1):
-        content = diagram[(r_i, c_room)]
+        content = diagram[get_index((r_i, c_room))]
         if content == ".":
             return [((r_i, c_room), r_i - 1 + abs(c_room - c))]
         if content != amphipod:
@@ -140,32 +107,18 @@ def get_legal_destinations(diagram, max_depth, amphipod, pos):
     raise Exception(f"seems to be too many of type {amphipod}")
 
 
-
 def get_legal_moves(diagram):
-    legal_moves = []
-    for pos in POSITIONS:
-        amphipod = diagram[pos]
-        if not amphipod in "ABCD":
-            continue
-        legal_moves += [(amphipod, pos, dest, dist) for dest, dist in get_legal_destinations(diagram, 2, amphipod, pos)]
-    return legal_moves
-def _get_legal_moves(diagram):
     legal_moves = []
     for pos in POSITIONS:
         amphipod = diagram[get_index(pos)]
         if not amphipod in "ABCD":
             continue
         legal_moves += [(amphipod, pos, dest, dist) for dest, dist in _get_legal_destinations(diagram, amphipod, pos, pos, pos, 0)]
+        #legal_moves += [(amphipod, pos, dest, dist) for dest, dist in get_legal_destinations(diagram, 2, amphipod, pos)]
     return legal_moves
 
 
 def apply_move(diagram, origin, destination):
-    new_diagram = {k: v for k, v in diagram.items()}
-    new_diagram[origin] = diagram[destination]
-    new_diagram[destination] = diagram[origin]
-    return new_diagram
-
-def _apply_move(diagram, origin, destination):
     index_orig = get_index(origin)
     index_dest = get_index(destination)
     amphipod = diagram[index_orig]
@@ -199,32 +152,15 @@ TEST = list("""#############
   #A#B#C#D#  
   #########  """.replace("\n", ""))
 
-assert TEST == _apply_move([c for c in DONE], (2, 3), (1, 1))
+assert TEST == apply_move([c for c in DONE], (2, 3), (1, 1))
 for amphipod, col in ROOM_NUMBER.items():
     assert DONE[get_index((2, col))] == amphipod
     assert DONE[get_index((3, col))] == amphipod
 
 
-def parse_diagram(text):
-    diagram = {}
-    for r, line in enumerate(text.splitlines()):
-        for c, symbol in enumerate(line):
-            if symbol in " #":
-                continue
-            diagram[(r, c)] = symbol
-    return diagram
-
-
-DONE = parse_diagram("""#############
-#...........#
-###A#B#C#D###
-  #A#B#C#D#  
-  #########  """)
-
-
 cache = {}
 def find_minimum_cost(diagram):
-    diagram_string = "".join(map(lambda k: str((k, diagram[k])), sorted(diagram.keys())))
+    diagram_string = "".join(diagram)
     if diagram_string in cache:
         return cache[diagram_string]
     if diagram == DONE:
@@ -251,31 +187,15 @@ def find_minimum_cost(diagram):
     cache[diagram_string] = minimum, moves
     return minimum, moves
 
-_amphipod_diagram = """#############
-#...........#
-###D#B#D#A###
-  #C#C#A#B#
-  #########"""
 
-assert not get_legal_destinations(parse_diagram("""
-#.D.........#
-###.#B#D#A###
-  #C#C#A#B#
-  #########"""), 2, "D", (1, 2))
-l = get_legal_destinations(parse_diagram("""
-#...........#
-###C#B#D#B###
-  #A#C#A#D#
-  #########"""), 2, "C", (2, 3))
-assert len(l) == 7
-
-cost, moves = find_minimum_cost(parse_diagram(_amphipod_diagram))
+cost, moves = find_minimum_cost(amphipod_diagram)
 print(cost)
-print(moves)
 
 
+"""
 print_diagram(amphipod_diagram)
 for orig, dest in moves:
-    amphipod_diagram = _apply_move(amphipod_diagram, orig, dest)
+    amphipod_diagram = apply_move(amphipod_diagram, orig, dest)
     print_diagram(amphipod_diagram)
     print()
+"""
