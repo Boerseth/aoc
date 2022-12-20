4fde192b2a9fd452480f06916f5f101368474cff
frode <frode.thorsen.borseth@softwarelab.no>
2022-12-20 08:15:09 +0100

    Seriously optimise day 20

--- 2022/20.py
+++ 2022/20.py
@@ -1,32 +1,30 @@
-def mix(positions):
-    N = len(positions)
-    for i in range(N):
-        number, prev = positions[i]
+def mix(numbers, indices):
+    N = len(numbers)
+    for i, number in enumerate(numbers):
+        prev = indices.index(i)
         curr = (prev + number - 1) % (N - 1) + 1
-        positions = [
-            (n, other - (prev < other <= curr) + (curr <= other < prev))
-            for n, other in positions
-        ]
-        positions[i] = (number, curr)
-    return positions
+        indices.pop(prev)
+        indices.insert(curr, i)
+    return indices
 
 
-def find_coordinates(positions):
-    N = len(positions)
-    zero_pos = next(pos for n, pos in positions if n == 0)
-    return [n for n, pos in positions if (pos - zero_pos) % N in [1000, 2000, 3000]]
+def find_coordinates(numbers, indices):
+    N = len(numbers)
+    zero_pos = indices.index(numbers.index(0))
+    coordinate_indices = [indices[(zero_pos + i) % N] for i in [1000, 2000, 3000]]
+    return [numbers[i] for i in coordinate_indices]
 
 
 def solve(text):
     numbers = [int(line) for line in text.splitlines()]
+    indices = list(range(len(numbers)))
+    yield sum(find_coordinates(numbers, mix(numbers, indices)))
 
-    positions = [(number, i) for i, number in enumerate(numbers)]
-    yield sum(find_coordinates(mix(positions)))
-
-    positions = [(number * 811589153, i) for i, number in enumerate(numbers)]
-    for i in range(10):
-        positions = mix(positions)
-    yield sum(find_coordinates(positions))
+    numbers = [n * 811589153 for n in numbers]
+    indices = list(range(len(numbers)))
+    for _ in range(10):
+        indices = mix(numbers, indices)
+    yield sum(find_coordinates(numbers, indices))
 
 
 if __name__ == "__main__":
