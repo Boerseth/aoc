b53d74236ab2e42d5faa9bca8c0664f96e6d651c
frode <frode.thorsen.borseth@softwarelab.no>
2023-10-30 20:19:31 +0100

    2015-4 typing

--- python/2015/4.py
+++ python/2015/4.py
@@ -1,16 +1,16 @@
 """The Ideal Stocking Stuffer"""
-
 import hashlib
+from typing import Iterator
 
 
-def miner(seed, goal):
+def miner(seed: str, goal: str) -> int:
     nonce = 0
     while not hashlib.md5(f"{seed}{nonce}".encode()).hexdigest().startswith(goal):
         nonce += 1
     return nonce
 
 
-def solve(text):
+def solve(text: str) -> Iterator[int]:
     text = text.strip()
     yield miner(text, "00000")
     yield miner(text, "000000")
