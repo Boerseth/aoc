7b0a4586be1e0b7722b135f7b3c10738f97e077c
frode <frode.thorsen.borseth@softwarelab.no>
2023-10-30 20:28:51 +0100

    2016 day 3

--- python/2016/3.py
+++ python/2016/3.py
@@ -1,7 +1,18 @@
-""""""
+"""Squares With Three Sides"""
 from typing import Iterator
 
 
+def is_possible(triangle: tuple[int, int, int]) -> bool:
+    return 2 * max(triangle) < sum(triangle)
+
+
 def solve(text: str) -> Iterator:
-    yield None
-    yield None
+    lines = [[int(i) for i in line.split()] for line in text.strip().splitlines()]
+    yield sum(is_possible(triangle) for triangle in lines)
+
+    triangles = [
+        triangle
+        for columns in zip(lines[::3], lines[1::3], lines[2::3])
+        for triangle in zip(*columns)
+    ]
+    yield sum(is_possible(triangle) for triangle in triangles)
