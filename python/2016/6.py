102a949e2c959a851cd5864c70d7d969c2f697e0
frode <frode.thorsen.borseth@softwarelab.no>
2023-10-30 20:28:51 +0100

    2016 day 6

--- python/2016/6.py
+++ python/2016/6.py
@@ -1,7 +1,21 @@
-""""""
+"""Signals and Noise"""
 from typing import Iterator
 
 
-def solve(text: str) -> Iterator:
-    yield None
-    yield None
+def get_counts(word: str) -> dict[str, int]:
+    counts = {}
+    for character in word:
+        counts[character] = counts.get(character, 0) + 1
+    return counts
+
+
+def solve(text: str) -> Iterator[str]:
+    rows = text.strip().splitlines()
+    columns = [*zip(*rows)]
+    all_counts = [get_counts(word) for word in columns]
+
+    # most frequent letter every column
+    yield "".join(max(counts, key=lambda c: counts[c]) for counts in all_counts)
+
+    # least frequent letter every column
+    yield "".join(min(counts, key=lambda c: counts[c]) for counts in all_counts)
