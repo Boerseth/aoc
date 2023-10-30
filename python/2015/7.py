32606b79a11f67a2131a5d179f43137f5f9b90bc
frode <frode.thorsen.borseth@softwarelab.no>
2023-10-30 20:19:50 +0100

    2015-7 structure

--- python/2015/7.py
+++ python/2015/7.py
@@ -1,80 +1,89 @@
 """Some Assembly Required"""
 
+
+class Circuit:
+    def __init__(self):
+        self.potentials = {}
+
+    def get(self, key: str) -> int | None:
+        if key.isdigit():
+            return int(key)
+        return self.potentials.get(key)
+
+    def _not(self, source: str) -> None:
+        if val := self.get(source):
+            return ~ val
+
+    def _and(self, source1: str, source2: str) -> int | None:
+        val1 = self.get(source1)
+        val2 = self.get(source2)
+        if val1 is not None and val2 is not None:
+            return val1 & val2
+
+    def _or(self, source1: str, source2: str) -> int | None:
+        val1 = self.get(source1)
+        val2 = self.get(source2)
+        if val1 is not None and val2 is not None:
+            return val1 | val2
+
+    def lshift(self, source1: str, source2: str) -> int | None:
+        val1 = self.get(source1)
+        val2 = self.get(source2)
+        if val1 is not None and val2 is not None:
+            return (val1 << val2) % 2 ** 16
+
+    def rshift(self, source1: str, source2: str) -> int | None:
+        val1 = self.get(source1)
+        val2 = self.get(source2)
+        if val1 is not None and val2 is not None:
+            return (val1 >> val2) % 2 ** 16
+
+    def set(self, key: str, value: int | None) -> None:
+        if value is not None:
+            self.potentials[key] = value
+
+    def turn_on_potentials(self, instructions: list[tuple[str, str]]) -> None:
+        iterations_since_last_update = 0
+        while instructions and iterations_since_last_update <= len(instructions):
+            expression, target = instructions.pop(0)
+            if self.get(target) is not None:
+                continue
+            match expression.split():
+                case source,:
+                    self.set(target, self.get(source))
+                case "NOT", source:
+                    self.set(target, self._not(source))
+                case source1, "AND", source2:
+                    self.set(target, self._and(source1, source2))
+                case source1, "OR", source2:
+                    self.set(target, self._or(source1, source2))
+                case source1, "LSHIFT", source2:
+                    self.set(target, self.lshift(source1, source2))
+                case source1, "RSHIFT", source2:
+                    self.set(target, self.rshift(source1, source2))
+                case _:
+                    pass
+            if self.get(target) is None:
+                instructions.append((expression, target))
+                iterations_since_last_update += 1
+            else:
+                iterations_since_last_update = 0
+
+        # Remaining wires are not hooked up to anything. Define them as grounded.
+        for _, target in instructions:
+            self.set(target, 0)
+        # But really, all instructions are used:
+        assert not instructions
+
+
 def solve(text):
     instructions = [line.strip().split(" -> ") for line in text.splitlines()]
 
-    # The keys of this dict are the wire-names, and its values will be their signal.
-    # Looping through the expressions, the dict will gradually fill up as we know
-    # more and more.
-    solved_circuit = {}
+    circuit1 = Circuit()
+    circuit1.turn_on_potentials([i for i in instructions])
+    yield circuit1.get("a")
 
-    def solved(key):
-        return key in solved_circuit
-
-    def store(key, value):
-        if key in solved_circuit:
-            pass
-        solved_circuit[key] = value
-
-    def get_value(key):
-        return solved_circuit[key]
-
-    def evaluate_1_element(element):
-        if element.isdigit():
-            return int(element)
-        if solved(element):
-            return get_value(element)
-        return None
-
-    def evaluate_2_elements(operator, x):
-        assert operator == "NOT"
-        if x.isdigit():
-            store(x, int(x))
-        if solved(x):
-            return ~ get_value(x)
-        return None
-
-    def evaluate_3_elements(x, operator, y):
-        assert operator in ["AND", "OR", "LSHIFT", "RSHIFT"]
-        if x.isdigit():
-            store(x, int(x))
-        if y.isdigit():
-            store(y, int(y))
-        if not solved(x) or not solved(y):
-            return None
-
-        if operator == "AND":
-            return get_value(x) & get_value(y)
-        if operator == "OR":
-            return get_value(x) | get_value(y)
-        if operator == "LSHIFT":
-            return (get_value(x) << int(y)) % 2 ** 16
-        if operator == "RSHIFT":
-            return (get_value(x) >> int(y)) % 2 ** 16
-        return None
-
-    def try_to_evaluate(expression):
-        expression_parts = expression.split()
-        if len(expression_parts) == 1:
-            return evaluate_1_element(expression)
-        if len(expression_parts) == 2:
-            return evaluate_2_elements(*expression_parts)
-        assert len(expression_parts) == 3
-        return evaluate_3_elements(*expression_parts)
-
-    # Part 1
-    while "a" not in solved_circuit:
-        for expression, assigned in instructions:
-            if not solved(assigned):
-                if (value := try_to_evaluate(expression)) is not None:
-                    store(assigned, int(value) % 2 ** 16)
-    yield solved_circuit["a"]
-
-    # Part 2
-    solved_circuit = {"b": solved_circuit["a"]}
-    while "a" not in solved_circuit:
-        for expression, assigned in instructions:
-            if not solved(assigned):
-                if (value := try_to_evaluate(expression)) is not None:
-                    store(assigned, int(value) % 2 ** 16)
-    yield solved_circuit["a"]
+    circuit2 = Circuit()
+    circuit2.set("b", circuit1.get("a"))
+    circuit2.turn_on_potentials([i for i in instructions])
+    yield circuit2.get("a")
